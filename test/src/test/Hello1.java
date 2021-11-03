331f534738057354c598e4089d8dd3b5bca2f5fd
user <user@DESKTOP-4NMVE1B>
2021-11-03 17:19:25 +0900

    test

---
+++ test/src/test/Hello1.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+//http://docs.oracle.com/javase/8/docs/api
+//		i = 10;
+//		i = "이숭무"
+		int i = 10;
+		String str = "이숭무";
+		System.out.println(i);
+		System.out.println(str);
+		// i = "이숭무" 위에서 숫자만 대입한다고 선언해서 안됨
+		int y; //변수 선언 후 나중에 값 대입 가능
+		y = 20;
+		System.out.println(y);
+		
+	}
+
+}
